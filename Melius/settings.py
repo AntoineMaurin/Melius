2b031d73552fb5caa074ed6114120e94e2ca7dca
AntoineMaurin <antoine.maurin26@gmail.com>
2020-08-11 17:48:55 +0200

    Ajout de l'app accounts et des pages de connexion / inscription

--- Melius/settings.py
+++ Melius/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'Melius.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'Melius.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Melius',
+        'USER': 'student',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
---
+++ accounts/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from accounts.forms import UserRegisterForm, UserLoginForm
+from accounts.register_management import RegisterManagement
+
+
+def user_login(request):
+    form = UserLoginForm(request.POST)
+    if request.method == 'POST':
+        email = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            login(request, user)
+            request.session['user_mail'] = user.email
+            request.session['user_name'] = user.email.split("@")[0]
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("/tasks")
+        else:
+            messages.error(request, "Email ou mot de passe incorrect.")
+            return render(request, "login.html", {'form': form})
+    else:
+        form = UserLoginForm()
+        return render(request, "login.html", {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password1 = form.cleaned_data.get('pw1')
+            password2 = form.cleaned_data.get('pw2')
+
+            rm = RegisterManagement(email=email, pw1=password1, pw2=password2)
+            message = rm.analyze()
+            if message[0] == 'error':
+                messages.error(request, message[1])
+                form = UserRegisterForm(request.POST)
+                return render(request, "register.html", {'form': form})
+            else:
+                User.objects.create_user(username=email,
+                                         email=email,
+                                         password=password1)
+                messages.success(request, message[1])
+                return redirect("/tasks")
+    else:
+        form = UserRegisterForm()
+    return render(request, "register.html", {'form': form})
