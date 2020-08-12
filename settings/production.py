881622088c06abaae6b4097b95a3836f12d1a7a6
Antoine Maurin <antoine.maurin26@gmail.com>
2020-08-12 18:56:01 +0200

    Mise en place de sentry pour la production

--- settings/production.py
+++ settings/production.py
@@ -2,7 +2,18 @@ import django_heroku
 import os
 
 from Melius.settings import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+
+sentry_sdk.init(
+    dsn="https://d25c81241686407b82c5e0fd1dcd00d5@o417103.ingest.sentry.io/5388294",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
